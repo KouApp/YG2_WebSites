bf464773c88a723be5cc72de141b3b73582feb28
emrecetindemir <55704322+emrecetindemir@users.noreply.github.com>
2022-04-02 11:12:50 +0300

    404 tasarim
    404 tasarımı yapıldı ve assetin içine bir css dosyası oluşturuldu

--- public/404.php
+++ public/404.php
@@ -9,23 +9,23 @@ header("Refresh: 5; url=login.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&family=Roboto:ital,wght@0,400;0,500;1,300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/404.css">
-    <link rel="stylesheet" href="css/menu.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,900" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="assets/css/style.css" />
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 </head>
 
 <body>
-<div id="navigation">
-    <script src = "menu.js"></script>
-</div>
 <div class="container">
-    <div class="content">
-        <h2>404</h2>
-        <h4>Üzgünüz! Aradığın sayfayı bulamadık :(</h4>
-        <p>5 saniye içerisinde otomatik olarak Ana Sayfa'ya yönlendirileceksin.</p>
-        <a href="login.php">Ana Sayfa'ya Dön</a>
+    <div id="notfound">
+        <div class="notfound">
+            <div class="notfound-404">
+                <h1>Oops!</h1>
+            </div>
+            <h2>404 - Page not found</h2>
+            <p>Aradığınız sayfa, adı değiştirilmiş veya geçici olarak kullanılamıyor olabilir. 5 saniye sonra otomatik olarak Ana Sayfa'ya yönlendirileceksiniz.</p>
+            <a href="login.php">Ana Sayfa'ya Dön</a>
+        </div>
     </div>
 </div>
 </body>
